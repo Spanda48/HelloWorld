b7836bc02588f89be11805d9acc751b5e51bbf48
sucheta <suchetapanda48@gmail.com>
2021-11-18 17:06:28 -0500

    my first implementation

---
+++ src/com/sucheta/Hello.java
@@ -0,0 +1,12 @@
+package com.sucheta;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("my first git");
+
+	}
+
+}
